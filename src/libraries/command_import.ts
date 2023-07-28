ec3d5581e5587e90d7432e668463cb7eeb26cbc8
TNTzx <azelynnrituapersonality@gmail.com>
2023-07-28 14:04:31 +0800

    added set admin

--- src/libraries/command_import.ts
+++ src/libraries/command_import.ts
@@ -1,13 +1,15 @@
 import * as DjsTools from "djs-tools"
 
 import * as Basic from "./basic"
+import * as Moderation from "./moderation"
 
 import Test from "./test/test"
 
 
 
 let commandList: DjsTools.CmdTemplateType[] = [
-    Basic.cmdHello
+    Basic.cmdHello,
+    Moderation.cmdSetAdmin
 ]
 
 
